4ab8fa8de1d14b1d2a917695889efa2857292429
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-10-18 14:45:36 +0000

    basic done

--- index.py
+++ index.py
@@ -3,11 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html', name = "sam")
+def home():
+    return render_template('index.html')
+    
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-@app.route("/blogs")
-def blogs():
-    return "<p>No blogs so far!</p>"
+@app.route("/post")
+def post():
+    return render_template('post.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 app.run(debug=True)
