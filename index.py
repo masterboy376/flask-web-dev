aa7917ddc6b0055fd4688665dbc65b69d86e7a9e
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-10-26 12:50:17 +0000

    displayed a sample blog .

--- index.py
+++ index.py
@@ -1,30 +1,24 @@
 from flask import Flask, render_template, request, jsonify
 import db
 from datetime import date
-from flask_mail import Mail
+# from flask_mail import Mail
 
 app = Flask(__name__)
-app.config.update(
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = '465',
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = db.params['gmail_username'],
-    MAIL_PASSWORD = db.params['gmail_password']
-)
-
-mail = Mail(app);
 
 @app.route("/")
 def home():
-    return render_template('index.html', params=db.params)
+    blogs = db.db.posts.find()
+    print(date.today())
+    return render_template('index.html', params=db.params, blogs=blogs)
     
 @app.route("/about")
 def about():
     return render_template('about.html', params=db.params)
 
-@app.route("/post")
-def post():
-    return render_template('post.html', params=db.params)
+@app.route("/post/<string:slug>")
+def post(slug):
+    blog = db.db.posts.find_one({"slug":slug})
+    return render_template('post.html', params=db.params, blog=blog)
 
 @app.route("/contact", methods=['GET','POST'])
 def contact():
@@ -34,10 +28,6 @@ def contact():
         phone = request.form.get('phone')
         message = request.form.get('message')
         db.db.queries.insert_one({"name": name, "email": email, "phone": phone, "message": message})
-        mail.send_message('New message from: '+name+' on Blog Blaze',
-                            sender=email,
-                            recipients=[db.params['gmail_username']],
-                            body=message+'\n phone:'+phone)
     return render_template('contact.html', params=db.params)
 
 
