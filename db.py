d65aa9bb577fceb5d7a35be35efc31f6b70cda12
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-10-31 11:31:35 +0000

    ignoring config.json

--- db.py
+++ db.py
@@ -12,5 +12,3 @@ else:
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('blog_blaze')
 # posts = pymongo.collection.Collection(db, 'posts')
-
-# mongodb+srv://masterboy376:S10a09m2004@samprivatecluster.ebbjt.mongodb.net/evergoods?retryWrites=true&w=majority
